20822399c8ebfd3753a29187998e30ae7acbc538
Arnold Patu <Arnold Patu@192.168.0.8>
2025-08-28 21:54:21 -0500

    carga inicial

--- src/proyectof/proyectof.java
+++ src/proyectof/proyectof.java
@@ -12,7 +12,7 @@ public class proyectof {
     public static void main(String[] args) {
         //variables
         String cliente;
-        double tc,compra1,compra2,compra3,pc;
+        double tc,compra1,compra2,compra3,pc,dc,tp;
         //creando las lecturas
         Scanner lectura=new Scanner(System.in);
         //Entrada de datos
@@ -28,10 +28,13 @@ public class proyectof {
         //procesos
         tc=compra1+compra2+compra3;
         //le hago q me de resultado con un decimal ya q son terminos monetarios
-        pc=Math.round(tc/3*10.0)/10.0;
-                
+        dc=tc*0.05;
+        tp=tc-dc;
+        pc=Math.round(tp/3*10.0)/10.0;
         //salida
-        System.out.println("El monto total de las compras de "+cliente+" es de: "+tc);
+        System.out.println("El monto previo de las compras de "+cliente+" es de: "+tc);
+        System.out.println("En descuento es de: "+dc);
+        System.out.println("En monto a pagar: "+tp);
         System.out.println("En promedio las compras suman un total de: "+pc);
                
     }
